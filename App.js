a0bd8f189caf15f7a2ac6bc87b1533cef646de6d
ts7752 <ts7752@naver.com>
2022-12-30 16:54:59 +0900

    tutorial

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [cnt, SetCnt] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.assa}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={styles.cnt}>{cnt}</Text>
+      <Button title="더하기" onPress={() => SetCnt(cnt + 15)}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  assa: {
+    color: "#f00000",
+    fontSize: 20,
+  },
+  cnt: {
+    fontSize: 50,
+    marginTop: 20,
   },
 });
